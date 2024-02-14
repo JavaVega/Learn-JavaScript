877d61a0f2cb2928a66f2e95144212928b17622c
JavaVega <jcarrascalgarcia>
2024-02-14 17:11:14 -0500

    Conocimiento de Array

---
+++ js/06.js
@@ -0,0 +1,12 @@
+//Teoria en Array 
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril","Mayo", "Junio"];
+
+console.log(meses);
+console.table(meses);
+
+
+//eL ForEach es una bucle que ayuda a ejecutar todo el array que tenga
+meses.forEach(function(meses){
+    console.log(meses)
+})
