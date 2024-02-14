8ee716942bdc4c972565555ea15e6dfb8f3c14ed
JavaVega <jcarrascalgarcia>
2024-02-14 18:30:55 -0500

    Nuevas metodos relacionado con los array

--- js/06.js
+++ js/06.js
@@ -10,3 +10,28 @@ console.table(meses);
 meses.forEach(function(meses){
     console.log(meses)
 })
+
+//Agregar una atributo al array
+
+//Con el metodo de .push agregara el dato al final del arreglo
+meses.push("Julio");
+
+//Con el metodo de .unshift agregara el dato al inicio del arreglo
+meses.unshift("Diciembre");
+
+//Con el metodo de .pop elimina el dato al final del arreglo
+meses.pop();
+
+//Con el metodo de .pop elimina el dato al final del arreglo
+meses.shift();
+
+
+//Con el metodo de .splice toma en cuenta dos datos  para eliminar
+
+meses.splice(2, 1);
+console.log(meses);
+
+//Rest operator o Spread operator
+
+let nuevoArreglo = [...meses, "Noviembre"];
+console.log(nuevoArreglo);
