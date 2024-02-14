0ef44724e11995d7875ac5d42caf9d3174f204d7
JavaVega <jcarrascalgarcia>
2024-02-14 14:20:35 -0500

    Teoria de JavaScript

---
+++ js/01.js
@@ -0,0 +1,11 @@
+const mensaje = "Ese es un mensaje de javaScript";
+
+//Aprenderas a manejar la funcion length
+console.log(mensaje.length);
+
+//metodo indexOf()
+console.log(mensaje.indexOf("mensaje"));
+
+//metodo include() para busqueda
+
+console.log(mensaje.includes("javaScript"));
---
+++ js/02.js
@@ -0,0 +1,10 @@
+//Aprender el objeto de Math
+
+let resultado;
+resultado = Math.PI;
+resultado = Math.ceil(2.5); //Aproxima el numero a 3 
+resultado = Math.floor(2.5); // Aproxima el numero a 2 siempre
+resultado = Math.round(Math.random()*10);// Formula para saber un numero random     
+
+
+console.log(resultado);
