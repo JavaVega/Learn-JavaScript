089f1d0c2be6b32cb97b194971262e68270c3ac9
JavaVega <jcarrascalgarcia>
2024-02-16 23:58:20 -0500

    Estructura del FOR loop

---
+++ js/10.js
@@ -0,0 +1,22 @@
+// Bucles - Loop
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril","Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// FOR Loop
+for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i]);
+}
+
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0 ){
+        console.log(`El numero ${i} es PAR`);
+    } else {
+        console.log(`El numero ${i} es IMPAR`);
+    }
+    
+}
+
+//While Loop
+
+
+//Do While Loop
